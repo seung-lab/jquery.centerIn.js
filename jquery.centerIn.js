f9a0863c8afbe5300c6c464a54d07c635a586d0f
William Silversmith <wms@mit.edu>
2014-02-11 23:52:38 -0500

    Converted indentation to spaces.

--- jquery.centerIn.js
+++ jquery.centerIn.js
@@ -16,107 +16,107 @@
  */
 ;(function($, undefined) {
 
-	/* centerIn
-	 *
-	 * Centers the element with respect to
-	 * the first element of the given selector
-	 * both horizontally and vertically.
-	 *
-	 * Required:
-	 *   [0] selector: The element to center within
-	 *   [1] options or callback
-	 *   [2] callback (if [1] is options): Mostly useful for alwaysCenterIn
-	 *
-	 * Options:
-	 *	 direction: 'horizontal', 'vertical', or 'both' (default)
-	 *	 top: Additional offset in px
-	 *	 left: Additional offset in px
-	 *
-	 * Returns: void
-	 */
-	$.fn.centerIn = function (selector, options, callback) {
-		var elements = this;
-		options = options || {};
+    /* centerIn
+     *
+     * Centers the element with respect to
+     * the first element of the given selector
+     * both horizontally and vertically.
+     *
+     * Required:
+     *   [0] selector: The element to center within
+     *   [1] options or callback
+     *   [2] callback (if [1] is options): Mostly useful for alwaysCenterIn
+     *
+     * Options:
+     *   direction: 'horizontal', 'vertical', or 'both' (default)
+     *   top: Additional offset in px
+     *   left: Additional offset in px
+     *
+     * Returns: void
+     */
+    $.fn.centerIn = function (selector, options, callback) {
+        var elements = this;
+        options = options || {};
 
-		if (typeof(options) === 'function') {
-			callback = options;
-			options = {};
-		}
+        if (typeof(options) === 'function') {
+            callback = options;
+            options = {};
+        }
 
-		var direction = options.direction || $.fn.centerIn.defaults.direction;
-		var extraleft = options.left || 0;
-		var extratop = options.top || 0;
+        var direction = options.direction || $.fn.centerIn.defaults.direction;
+        var extraleft = options.left || 0;
+        var extratop = options.top || 0;
 
-		if (selector) {
-			selector = $(selector).first();
-		}
-		else {
-			selector = elements.first().parent();
-		}
+        if (selector) {
+            selector = $(selector).first();
+        }
+        else {
+            selector = elements.first().parent();
+        }
 
-		try {
-			if (!selector.css('position') || selector.css('position') === 'static') {
-				selector.css('position', 'relative'); 
-			}
-		}
-		catch (e) {
-			// selector was something like window, document, html, or body
-			// which doesn't have a position attribute
-		}
+        try {
+            if (!selector.css('position') || selector.css('position') === 'static') {
+                selector.css('position', 'relative'); 
+            }
+        }
+        catch (e) {
+            // selector was something like window, document, html, or body
+            // which doesn't have a position attribute
+        }
 
-		var horizontal = function (element) {
-			var left = Math.round((selector.innerWidth() - element.outerWidth(false)) / 2);
-			left += translateDisplacement(selector, extraleft, 'width');
-			element.css('left', left + "px");
-		};
+        var horizontal = function (element) {
+            var left = Math.round((selector.innerWidth() - element.outerWidth(false)) / 2);
+            left += translateDisplacement(selector, extraleft, 'width');
+            element.css('left', left + "px");
+        };
 
-		var vertical = function (element) {
-			var top = Math.round((selector.innerHeight() - element.outerHeight(false)) / 2);
-			top += translateDisplacement(selector, extratop, 'height');
-			element.css('top', top + "px");
-		};
+        var vertical = function (element) {
+            var top = Math.round((selector.innerHeight() - element.outerHeight(false)) / 2);
+            top += translateDisplacement(selector, extratop, 'height');
+            element.css('top', top + "px");
+        };
 
-		var centerfn = constructCenterFn(horizontal, vertical, callback, direction);
+        var centerfn = constructCenterFn(horizontal, vertical, callback, direction);
 
-		elements.each(function (index, element) {
-			element = $(element);
+        elements.each(function (index, element) {
+            element = $(element);
 
-			if (element.css("position") !== 'fixed') {
-				element.css("position", 'absolute');
-			}
-			centerfn(element);
-		});
+            if (element.css("position") !== 'fixed') {
+                element.css("position", 'absolute');
+            }
+            centerfn(element);
+        });
 
-		return this;
-	};
+        return this;
+    };
 
-	/* alwaysCenterIn
-	 * 
-	 * Maintains centering even on window resize.
-	 */
-	$.fn.alwaysCenterIn = function () {
-		var args = arguments || []; 
-		var selector = $(this);
+    /* alwaysCenterIn
+     * 
+     * Maintains centering even on window resize.
+     */
+    $.fn.alwaysCenterIn = function () {
+        var args = arguments || []; 
+        var selector = $(this);
 
-		selector.centerIn.apply(selector, args);
+        selector.centerIn.apply(selector, args);
 
-		var evt = 'resize.centerIn';
+        var evt = 'resize.centerIn';
         if (selector.attr('id')) {
             evt += '.' + selector.attr('id');
         }
 
         $(window).on(evt, function () {
-			selector.centerIn.apply(selector, args);
-		});
+            selector.centerIn.apply(selector, args);
+        });
 
-		return this;
-	 };
+        return this;
+     };
 
-	/* Defaults */
+    /* Defaults */
 
-	$.fn.centerIn.defaults = {
-		direction: 'both'
-	};
+    $.fn.centerIn.defaults = {
+        direction: 'both'
+    };
 
     /* translateDisplacement
      *
@@ -159,79 +159,79 @@
      *
      * Returns: fn
      */
-	function constructCenterFn(horizontal, vertical, callback, direction) {
+    function constructCenterFn(horizontal, vertical, callback, direction) {
         var fns = []
 
-		if (!direction || direction === 'both') {
-			fns.push(vertical);
-            fns.push(horizontal);
-		}
-		else if (direction === 'horizontal') {
-            fns.push(horizontal);
-		}
-		else if (direction === 'vertical') {
+        if (!direction || direction === 'both') {
             fns.push(vertical);
-		}
+            fns.push(horizontal);
+        }
+        else if (direction === 'horizontal') {
+            fns.push(horizontal);
+        }
+        else if (direction === 'vertical') {
+            fns.push(vertical);
+        }
 
         if (callback) {
             fns.push(callback);
         }
 
-		return compose(fns);
-	}
+        return compose(fns);
+    }
 
-	/* compose
-	 *
-	 * Compose N functions into a single function call.
-	 *
-	 * Required: 
-	 *   [0-n] functions or arrays of functions
-	 * 
-	 * Returns: function
-	 */
-	function compose () {
-		var fns = flatten(arguments);
+    /* compose
+     *
+     * Compose N functions into a single function call.
+     *
+     * Required: 
+     *   [0-n] functions or arrays of functions
+     * 
+     * Returns: function
+     */
+    function compose () {
+        var fns = flatten(arguments);
 
-		return function () {
-			for (var i = 0; i < fns.length; i++) {
-				fns[i].apply(this, arguments);
-			}
-		};
-	}
+        return function () {
+            for (var i = 0; i < fns.length; i++) {
+                fns[i].apply(this, arguments);
+            }
+        };
+    }
 
-	/* flatten
-	 *
-	 * Take an array that potentially contains other arrays 
-	 * and return them as a single array.
-	 *
-	 * e.g. flatten([1, 2, [3, [4]], 5]) => [1,2,3,4,5]
-	 *
-	 * Required: 
-	 *   [0] array
-	 * 
-	 * Returns: array
-	 */
-	function flatten (array) {
-		array = array || [];
+    /* flatten
+     *
+     * Take an array that potentially contains other arrays 
+     * and return them as a single array.
+     *
+     * e.g. flatten([1, 2, [3, [4]], 5]) => [1,2,3,4,5]
+     *
+     * Required: 
+     *   [0] array
+     * 
+     * Returns: array
+     */
+    function flatten (array) {
+        array = array || [];
 
-		var flat = [];
+        var flat = [];
 
-		var len = array.length;
-		for (var i = 0; i < len; i++) {
-			var item = array[i];
+        var len = array.length;
+        for (var i = 0; i < len; i++) {
+            var item = array[i];
 
-			console.log(item, typeof(item));
+            console.log(item, typeof(item));
 
-			if (typeof(item) === 'object' && Array.isArray(item)) {
-				flat = flat.concat(flatten(item));
-			}
-			else {
-				flat.push(item);
-			}
-		} 
+            if (typeof(item) === 'object' && Array.isArray(item)) {
+                flat = flat.concat(flatten(item));
+            }
+            else {
+                flat.push(item);
+            }
+        } 
 
-		return flat;
-	}
+        return flat;
+    }
 })(jQuery);
 
 /* The MIT License (MIT)
